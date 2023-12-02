c77fba6853c17a0f58597d0a7281971d00553602
koko <k.cisek@gmail.com>
2023-12-02 23:03:54 +0000

    Refactor draw_gallows function.

--- milestone_5.py
+++ milestone_5.py
@@ -2,60 +2,40 @@ import random
 
 
 class Hangman:
-    
+
+
     def __init__(self, word_list, num_lives=5):
+
         self.word_list = word_list
         self.num_lives = num_lives
-        
+
         self.word = random.choice(word_list)
         self.word_guessed = ["_" for letter in self.word]
         self.num_letters = len(set(self.word))
         self.list_of_guesses = []
-    
+
     def draw_gallows(self):
-        if self.num_lives == 4:
-            print("\n")
-            print("    | ")
-            print("    | ")
-            print("-----")
-        if self.num_lives == 3:
-            print("\n")
-            print("    | ")
-            print("    | ")
-            print("    | ")
-            print("    | ")
-            print("-----")
-        if self.num_lives == 2:
-            print("\n")
-            print("  __ ")
-            print("    | ")
-            print("    | ")
-            print("    | ")
-            print("    | ")
-            print("-----")
-        if self.num_lives == 1:
-            print("\n")
-            print("  __ ")
-            print(" |  | ")
-            print("    | ")
-            print("    | ")
-            print("    | ")
-            print("-----")
-        if self.num_lives == 0:
-            print("\n")
-            print("  __ ")
-            print(" |  | ")
-            print(" o  | ")
-            print("/|\ | ")
-            print("/ \ | ")
-            print("-----")
- 
+        """_summary_
+        """
+        gallows = [
+            ["", "  __ ", " |  | ", " o  | ", "/|\ | ", "/ \ | ", "-----"],
+            ["", "  __ ", " |  | ", "    | ", "    | ", "    | ", "-----"],
+            ["", "  __ ", "    | ", "    | ", "    | ", "    | ", "-----"],
+            ["","    | ", "    | ", "    | ", "    | ", "-----"],
+            ["", "", "", "", "    | ", "    | ", "-----"],
+        ]
+
+        for i in range(len(gallows[self.num_lives])):
+            print(gallows[self.num_lives][i])
+
     def check_guess(self, guess):
+
         guess = guess.lower()
-        
+
         if guess in self.word:
             print(f"\nGood guess! '{guess}' is in the word.")
-            self.word_guessed = [guess if letter == guess else guessed_letter for letter, guessed_letter in zip(self.word, self.word_guessed)]
+            self.word_guessed = [guess if letter == guess else guessed_letter for letter,
+                                 guessed_letter in zip(self.word, self.word_guessed)]
             self.num_letters -= 1
         else:
             print(f"\nSorry, '{guess}' is not in the word.")
@@ -75,12 +55,12 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print("\nYou already tried that letter!")
                 continue
-            else: 
+            else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
                 break
-    
-              
+
+
 word_list = ["apple", "redcurrant", "blueberry", "raspberry", "strawberry"]
 
 
@@ -89,7 +69,7 @@ def game_intro(lives, guess_word):
     print("\n__________________\n")
     print("Let's start!")
     print("\n__________________\n")
-    print(f"The word is a fruit and has {len(guess_word)} letters:")  
+    print(f"The word is a fruit and has {len(guess_word)} letters:")
     print(f"\nWord: {' '.join(guess_word)}\n")
     print(f"You have {lives} lives.\n")
 
@@ -97,19 +77,19 @@ def game_intro(lives, guess_word):
 def play_game(word_list):
     num_lives = 5
     game = Hangman(word_list, num_lives)
-    
+
     game_intro(num_lives, game.word_guessed)
-    
+
     while True:
         if game.num_lives == 0:
             print("\nYou lost!")
             break
         if game.num_letters > 0:
             game.ask_for_input()
-        if game.num_lives != 0 and game.num_letters == 0: 
+        if game.num_lives != 0 and game.num_letters == 0:
             print("\nCongratulations, you won!")
             break
-        
+
 
 if __name__ == "__main__":
     play_game(word_list)
