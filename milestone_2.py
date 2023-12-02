8cfb17243dcc580035a37db96b7a595e4e984ee5
koko <k.cisek@gmail.com>
2023-12-02 10:09:46 +0000

    Refactor: add ask_user_for_input func; remove get_guess_from_user.

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,10 @@
 import random
 
-# Set up
+
+# Set up 
 word_list = ["apple", "redcurrant", "blueberry", "raspberry", "strawberry"]
 print(word_list)
+
 
 def draw_word_for_game(word_list):
     return random.choice(word_list)
@@ -15,22 +17,15 @@ def validate_if_single_letter(guess):
         raise ValueError("You you need to provide a single letter.")
         
 
-def get_guess_from_user():
+def ask_user_for_input(prompt):
     """Ask user to select the item"""
-    while True:
-        guess = input("What letter are you thinking of? ")
-        try:
-            validate_if_single_letter(guess)
-            return guess
-        except ValueError as error:
-            print(f"{error} Try again.\n")
-            continue
-    
-        
-def run():
-    word = draw_word_for_game(word_list)
-    guess = get_guess_from_user()
-    
+    return input(prompt)
 
+
+
+# For testing
 if __name__ == "__main__":
-    run()
+    word = draw_word_for_game(word_list)
+    prompt = "Provide letter "
+    ask_user_for_input(prompt)
+    
